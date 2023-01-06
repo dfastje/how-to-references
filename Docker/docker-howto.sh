cd1102b97d31223026e9c43ce4c172a24401064f
dfastje <dfastjework@protonmail.com>
2023-01-06 00:36:26 -0500

    minor updates docker-howto.sh

--- Docker/docker-howto.sh
+++ Docker/docker-howto.sh
@@ -1,2 +1,8 @@
 #Running a container. Will pull it down from Docker's repo
-  #sudo docker run -d --restart=unless-stopped -p <HostPort>:<ContainerPort> <ContainerGroup>/<ContainerName>
+  #docker run -d --restart=unless-stopped -p <HostPort>:<ContainerPort> <ContainerGroup>/<ContainerName>
+
+#List containers:
+  docker container ls
+
+#Stop containers:
+  docker stop my_container
